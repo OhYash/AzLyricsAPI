6478012d3f75d0c80d9c76f9ec6ef2119c7d69f3
Yash Yadav <yashdimpu@gmail.com>
2018-03-05 13:42:49 +0530

    Fixed link and parser option

--- api/azapi.py
+++ api/azapi.py
@@ -24,15 +24,15 @@ from bs4 import BeautifulSoup
 import urllib.request, urllib.error, urllib.parse
 
 def generating(artist, title, save):
-        artist = artist.lower().replace(" ", "%20")
-        title = title.lower().replace(" ", "%20")
+        artist = artist.lower().replace(" ", "")
+        title = title.lower().replace(" ", "")
         generate_url = 'http://azlyrics.com/lyrics/'+artist+'/'+title +'.html'
         processing(generate_url, artist, title, save)
         
 def processing(generate_url, artist, title, save):
     response = urllib.request.urlopen(generate_url)
     read_lyrics = response.read()
-    soup = BeautifulSoup(read_lyrics)
+    soup = BeautifulSoup(read_lyrics, "html.parser")
     lyrics = soup.find_all("div", attrs={"class": None, "id": None})
     lyrics = [x.getText() for x in lyrics]
     printing(artist, title, save, lyrics)
